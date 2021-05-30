f48c60b721ebf9d2d7f9f5378df55c5023ee0338
chun37 <chun.jb.37@gmail.com>
2021-05-31 05:30:29 +0900

    まっさらに生まれ変わって人生1から始めようか

--- tagging/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from tagging.views import tag, views
-
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("tags/create/", tag.Create.as_view()),
-    path("tags/", tag.List.as_view()),
-]
