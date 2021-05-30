880e7fd89d9e41612b54812c1dfec366629d4939
chun37 <chun.jb.37@gmail.com>
2021-05-31 08:16:31 +0900

    カスタムユーザーで、adminにログイン出来る

--- account/models.py
+++ account/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+class Twitter(models.Model):
+    screen_name = models.CharField(max_length=255, default="")
+    display_name = models.CharField(max_length=255, default="")
+
+    access_token = models.CharField(max_length=255)
+    access_token_secret = models.CharField(max_length=255)
+
+    raw = models.TextField()
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, password, **extra_fields):
+        user = self.model(**extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, **extra_fields):
+        return self._create_user(**extra_fields)
+
+    def create_superuser(self, **extra_fields):
+        extra_fields.setdefault("is_admin", True)
+
+        return self._create_user(**extra_fields)
+
+
+class User(AbstractBaseUser):
+    id = models.BigIntegerField(primary_key=True)
+    twitter = models.OneToOneField(Twitter, on_delete=models.CASCADE, null=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "id"
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
